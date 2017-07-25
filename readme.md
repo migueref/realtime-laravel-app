57cc2e26941a6a315f4609985536f47009e54930
migueref <reafmiguel@gmail.com>
2017-07-24 22:44:01 -0500

    testing Vue and first route

---
+++ readme.md
@@ -0,0 +1,15 @@
+Simple Realtime app built with laravel 5.4
+=====================
+
+Simple real time app to learn basic principles about real time applications.
+
+This repository was built with the following technologies:
+
+  - PHP
+  - Laravel 5.4
+  - Vue JS
+
+
+## Connect with me
+
+Linkedin https://www.linkedin.com/in/migueref
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Chatroom</title>
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+    <div id="app">
+      <example></example>
+      <h1>Best latin chat on earth</h1>
+    </div>
+    <script src="js/app.js" charset="utf-8"></script>
+  </body>
+</html>
